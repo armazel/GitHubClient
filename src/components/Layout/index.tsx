5aaa5bd9a5a48595176f1d4dbe08d82218f4ba12
egor dragun <dragun.egor@gmail.com>
2020-09-13 17:38:30 +0300

    add source map into webpack config

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-
+import * as styles from "../../styles/App.less";
 export const LayoutWrapper: React.FC = () => {
     return (
-        <h1>Hello</h1>
+        <h1 className={styles.wrapper}>Hello</h1>
     )
 }
